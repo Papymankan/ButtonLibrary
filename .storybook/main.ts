aa744ad7c108c7b4f9d33ce049be14593352dd9d
Papymankan <parsa84rostami@gmail.com>
2025-04-19 19:28:37 +0330

    remove comments

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,35 +1,19 @@
-// import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite';
 
-// const config: StorybookConfig = {
-//   "stories": [
-//     "../src/**/*.mdx",
-//     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-//   ],
-//   "addons": [
-//     "@storybook/addon-essentials",
-//     "@storybook/addon-onboarding",
-//     "@chromatic-com/storybook",
-//     "@storybook/experimental-addon-test"
-//   ],
-//   "framework": {
-//     "name": "@storybook/react-vite",
-//     "options": {}
-//   }
-// };
-// export default config;
-
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default {
-  stories: ["../src/**/*.stories.tsx"],
-  typescript: {
-    check: true,
-    checkOptions: {
-      tsconfig: path.resolve(__dirname, "../tsconfig.stories.json"),
-    },
-  },
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
 };
+export default config;
